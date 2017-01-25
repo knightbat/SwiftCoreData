c78e089f13442f307793c382ecf8f6e9fc212371
Bindu <bindu@xminds.in>
2017-01-25 16:05:39 +0530

    Initial Commit

---
+++ TestCoreSwift/PersonTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PersonTableViewCell.swift
+//  TestCoreSwift
+//
+//  Created by Bindu on 25/01/17.
+//  Copyright © 2017 Xminds. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
